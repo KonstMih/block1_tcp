7935c01ccd38671e80f95364b4a9253bee332963
Konstmih <konstmih.vod@gmail.com>
2023-03-15 09:42:19 +0300

    transform for block 1

--- devices/devices.go
+++ devices/devices.go
@@ -12,133 +12,88 @@ func new_device(adress int, name string, qantity int32, req []byte, ch []string)
 	return Device{Id: adress, Name: name, Qan_chanels: qantity, Request: req, Chanels: ch}
 }
 
-// ============================================================================Котёл 2А============================================================================
+// ============================================================================Котёл 1============================================================================
 
-var k2a_1 = []string{"Q_per_par_3a", "Q_pit_voda_3a"}
+var k1_1 = []string{"o2_slev"}
 
-var k2a_2 = []string{"P_para_2a", "T_para_nitk_A_2a", "T_para_nitk_B_2a"}
+var k1_2 = []string{"o2_sprav"}
 
-var k2a_3 = []string{"T_pitvod_kotl_2a", "T_mazut_2a", "T_uhodgaz_ventur1_2a", "T_uhodgaz_ventur2_2a", "T_uhodgaz_ventur3_2a", "T_uhodgaz_ventur4_2a"}
+var k1_3 = []string{"q_gaz"}
 
-var k2a_4 = []string{"T_vozdh_pered_rvp_2a", "T_vozdh_pered_tvpslev_2a", "T_vozdh_pered_tvpsprav_2a"}
+var k1_4 = []string{"t_para_nitk_a", "t_para_nitk_b", "p_para"}
 
-var k2a_5 = []string{"T_vozdh_posl_rvp_2a", "T_vozdh_posl_tvpslev_2a", "T_vozdh_posl_tvpsprav_2a", "T_perd_ekonomiz_2a",
-	"T_dym_zarvp_2a", "T_dym_zatvp_slev_2a", "T_dym_zatvp_sprav_2a"}
+var k1_5 = []string{"q_per_par", "q_pit_voda"}
 
-var k2a_6 = []string{"O2_slev_2a"}
+var k1_6 = []string{"t_vozdh_posl_rvpa", "t_vozdh_posl_rvpb", "t_vozdh_posl_tvp_slev", "t_vozdh_posl_tvp_sprav", "t_posle_ekonomiz_slev",
+	"t_posle_ekonomiz_sprav", "t_dym_zarvp_slev", "t_dym_zarvp_sprav", "t_dym_zatvp_slev", "t_dym_zatvp_sprav"}
 
-var k2a_7 = []string{"O2_sprav_2a"}
+var k1_7 = []string{"t_pitvod_kotl", "t_uhodgaz_ventur1", "t_uhodgaz_ventur2", "t_mazut", "t_uhodgaz_ventur3", "t_uhodgaz_ventur4"}
 
-var k2a_8 = []string{"Q_gaz_2a"}
+var k1_8 = []string{"t_kontensat_sbor", "t_kontensat_vihkollect", "t_vod_kollect_slev", "t_vod_kollect_sprav", "t_gaz_dsa", "t_gaz_dsb"}
 
-// ============================================================================Котёл 2Б============================================================================
+var k1_9 = []string{"t_vozdh_pered_rvpa", "t_vozdh_pered_rvpb", "t_vozdh_pered_tvpslev", "t_vozdh_pered_tvpsprav"}
 
-var k2b_1 = []string{"Q_per_par_2b", "Q_pit_voda_2b"}
+// ============================================================================Турбина 1============================================================================
 
-var k2b_2 = []string{"P_para_2b", "T_para_nitk_A_2b", "T_para_nitk_B_2b"}
+var t1_1 = []string{"p_par_do_sk"}
 
-var k2b_3 = []string{"T_pitvod_kotl_2b", "T_mazut_2b", "T_uhodgaz_ventur1_2b", "T_uhodgaz_ventur2_2b", "T_uhodgaz_ventur3_2b", "T_uhodgaz_ventur4_2b"}
+var t1_2 = []string{"q_par"}
 
-var k2b_4 = []string{"T_vozdh_pered_rvp_2b", "T_vozdh_pered_tvp_2b"}
+var t1_3 = []string{"t_par_posle_sk"}
 
-var k2b_5 = []string{"T_vozdh_posl_rvp_2b", "T_vozdh_posl_tvpslev_2b", "T_vozdh_posl_tvpsprav_2b", "T_perd_ekonomiz_2b",
-	"T_dym_zarvp_2b", "T_dym_zatvp_slev_2b", "T_dym_zatvp_sprav_2b"}
+var t1_4 = []string{"vakum"}
 
-var k2b_6 = []string{"O2_slev_2b"}
+var t1_5 = []string{"t_par_psg1_prov1", "t_par_psg1_prov2", "t_par_psg2", "t_vod_do_psg1", "t_vod_posle_psg1", "t_vod_vihod_psg12"}
 
-var k2b_7 = []string{"O2_sprav_2b"}
+var t1_6 = []string{"t_par_posle_ou1", "t_par_posle_ou2", "t_kondesat_do kn", "t_cirk_vod_posle_kondensat", "t_cirk_vod_do_kondensat",}
 
-var k2b_8 = []string{"Q_gaz_2b"}
-
-// ============================================================================Турбина 2============================================================================
-
-var t2_1 = []string{"T_ostrpar_pered_2GPZ2_nitka_A"}
-
-var t2_2 = []string{"T_ostrpar_pered_2GPZ2_nitka_B"}
-
-var t2_3 = []string{"P_para_do_stopor_klapan_nitka_A"}
-
-var t2_4 = []string{"P_para_do_stopor_klapan_nitka_B"}
-
-var t2_5 = []string{"Q_para_turbina_nitka_A"}
-
-var t2_6 = []string{"Q_para_turbina_nitka_B"}
-
-var t2_7 = []string{"vakum"}
-
-var t2_8 = []string{"T_cirk_voda_k_kondesator_N1", "T_cirk_voda_k_kondesator_N2", "T_cirk_voda_mezh_kondesator_N1", "T_cirk_voda_mezh_kondesator_N2",
-	"T_cirk_voda_sliv_N1", "T_cirk_voda_sliv_N2", "T_maslo_do_MO", "T_ohl_voda_do_MO"}
-
-var t2_9 = []string{"T_kondensat_posle_PND4", "T_set_voda_do_PSG1_0m", "T_set_voda_do_PSG1_3m", "T_set_voda_za_PSG1", "T_set_voda_za_PSG2", "T_cirk_voda_vstroenih_puchkov"}
-
-var t2_10 = []string{"P_para_psg1"}
-
-var t2_11 = []string{"P_para_psg2"}
+var t1_7 = []string{"t_par_cnd_sprav", "t_par_cnd_slev", "t_par_uplotn_kollect", "t_kondest_posle_pnd4", "t_vod_obvod_pvd7", 
+    "t_par_pered_pn130", "t_vod_posle_pvd7"}
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //
 // ============================================================================ИРТ5920Н============================================================================
 
-//var irt_1 = new_device(1, "irt5920n", 1, []byte{0xFF, 0x3A, 0x31, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x37, 0x36, 0x32, 0x37, 0x0D}, k2a_6) // панель:3-2N  котёл:2A
+var irt_1 = new_device(1, "irt5920n", 1, []byte{0xFF, 0x3A, 0x31, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x37, 0x36, 0x32, 0x37, 0x0D}, k1_1) // панель:17-1N  котёл:1
 
-//var irt_2 = new_device(2, "irt5920n", 1, []byte{0xFF, 0x3A, 0x32, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x31, 0x31, 0x39, 0x37, 0x39, 0x0D}, k2a_7) // панель:3-2N  котёл:2A
+var irt_2 = new_device(2, "irt5920n", 1, []byte{0xFF, 0x3A, 0x32, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x31, 0x31, 0x39, 0x37, 0x39, 0x0D}, k1_2) // панель:17-1N  котёл:1
 
-//var irt_3 = new_device(3, "irt5920n", 1, []byte{0xFF, 0x3A, 0x33, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x36, 0x35, 0x34, 0x38, 0x32, 0x0D}, k2a_8) // панель:3-2N  котёл:2A
+var irt_3 = new_device(3, "irt5920n", 1, []byte{0xFF, 0x3A, 0x33, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x36, 0x35, 0x34, 0x38, 0x32, 0x0D}, k1_3) // панель:1-1N  котёл:1
 
-var irt_4 = new_device(4, "irt5920n", 1, []byte{0xFF, 0x3A, 0x34, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x31, 0x38, 0x36, 0x33, 0x35, 0x0D}, k2b_6) // панель:3-3N  котёл:2Б
+var irt_4 = new_device(3, "irt5920n", 1, []byte{0xFF, 0x3A, 0x33, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x36, 0x35, 0x34, 0x38, 0x32, 0x0D}, t1_1) // панель:3-1S  турбина:1
 
-//var irt_5 = new_device(5, "irt5920n", 1, []byte{0xFF, 0x3A, 0x35, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x33, 0x39, 0x33, 0x37, 0x30, 0x0D}, k2b_7) // панель:3-3N  котёл:2Б
+var irt_5 = new_device(4, "irt5920n", 1, []byte{0xFF, 0x3A, 0x34, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x31, 0x38, 0x36, 0x33, 0x35, 0x0D}, t1_2) // панель:3-1S  турбина:1
 
-var irt_6 = new_device(6, "irt5920n", 1, []byte{0xFF, 0x3A, 0x36, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x34, 0x33, 0x37, 0x32, 0x32, 0x0D}, k2b_8) // панель:3-3N  котёл:2Б
-
-var irt_7 = new_device(7, "irt5920n", 1, []byte{0xFF, 0x3A, 0x37, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x33, 0x31, 0x36, 0x39, 0x31, 0x0D}, t2_10) // панель:2S-2  турбина:2
-
-//var irt_8 = new_device(8, "irt5920n", 1, []byte{0xFF, 0x3A, 0x38, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x33, 0x33, 0x39, 0x39, 0x35, 0x0D}, t2_11) // панель:2S-2  турбина:2
-
-var irt_9 = new_device(9, "irt5920n", 1, []byte{0xFF, 0x3A, 0x39, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x32, 0x31, 0x39, 0x36, 0x32, 0x0D}, t2_1) // панель:2S-2  турбина:2
-
-var irt_10 = new_device(10, "irt5920n", 1, []byte{0xFF, 0x3A, 0x31, 0x30, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x35, 0x33, 0x36, 0x31, 0x0D}, t2_2) // панель:2S-2  турбина:2
-
-var irt_11 = new_device(11, "irt5920n", 1, []byte{0xFF, 0x3A, 0x31, 0x31, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x35, 0x30, 0x36, 0x37, 0x32, 0x0D}, t2_3) // панель:2S1  турбина:2
-
-var irt_12 = new_device(12, "irt5920n", 1, []byte{0xFF, 0x3A, 0x31, 0x32, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x36, 0x33, 0x32, 0x31, 0x36, 0x0D}, t2_4) // панель:2S1  турбина:2
-
-var irt_13 = new_device(13, "irt5920n", 1, []byte{0xFF, 0x3A, 0x31, 0x33, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x31, 0x30, 0x32, 0x32, 0x35, 0x0D}, t2_5) // панель:2S1  турбина:2
-
-var irt_14 = new_device(14, "irt5920n", 1, []byte{0xFF, 0x3A, 0x31, 0x34, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x33, 0x37, 0x31, 0x30, 0x34, 0x0D}, t2_6) // панель:2S1  турбина:2
+var irt_6 = new_device(5, "irt5920n", 1, []byte{0xFF, 0x3A, 0x35, 0x3B, 0x31, 0x3B, 0x30, 0x3B, 0x33, 0x39, 0x33, 0x37, 0x30, 0x0D}, t1_3) // панель:3-1S  турбина:1
 
 // ============================================================================Ф1772============================================================================
 
-//var F_1 = new_device(15, "f1772", 2, []byte{0x0f, 0x04, 0x02, 0x00, 0x00, 0x04, 0xf1, 0x5f}, k2a_1) // панель:2-2N  котёл:2А
+var f_1 = new_device(5, "f1772", 3, []byte{0x05, 0x04, 0x02, 0x00, 0x00, 0x06, 0x70, 0x34}, k1_4) // панель:16-1N  котёл:1
 
-//var F_2 = new_device(16, "f1772", 3, []byte{0x10, 0x04, 0x02, 0x00, 0x00, 0x06, 0x72, 0xf1}, k2a_2) // панель:1-2N  котёл:2А
+var f_2 = new_device(6, "f1772", 2, []byte{0x06, 0x04, 0x02, 0x00, 0x00, 0x04, 0xf1, 0xc6}, k1_5) // панель:1-1N  котёл:1
 
-var F_3 = new_device(17, "f1772", 2, []byte{0x11, 0x04, 0x02, 0x00, 0x00, 0x04, 0xf2, 0xe1}, k2b_1) // панель:2-3N  котёл:2Б
-
-var F_4 = new_device(18, "f1772", 3, []byte{0x12, 0x04, 0x02, 0x00, 0x00, 0x06, 0x73, 0x13}, k2b_2) // панель:1-3N  котёл:2Б
-
-var F_5 = new_device(19, "f1772", 1, []byte{0x13, 0x04, 0x02, 0x00, 0x00, 0x02, 0x73, 0x01}, t2_7) // панель:2U  турбина:2
+var f_3 = new_device(6, "f1772", 1, []byte{0x06, 0x04, 0x02, 0x00, 0x00, 0x02, 0x71, 0xc4}, t1_4) // панель:1-1S  турбина:1
 
 // ============================================================================ТМ5104Д============================================================================
 
-//var tm_1 = new_device(20, "tm5104d", 7, []byte{0x14, 0x03, 0x05, 0x00, 0x00, 0x0e, 0xc6, 0x07}, k2a_5) // панель:3a-2N  котёл:2A
-
-var tm_2 = new_device(21, "tm5104d", 7, []byte{0x15, 0x03, 0x05, 0x00, 0x00, 0x0e, 0xc7, 0xd6}, k2b_5) // панель:3a-3N  котёл:2Б
+var tm_1 = new_device(7, "tm5104d", 10, []byte{0x07, 0x03, 0x05, 0x00, 0x00, 0x14, 0x45, 0x6f}, k1_6) // панель:3a-1N  котёл:1
 
 // ============================================================================ТРМ138============================================================================
 
-//var trm_1 = new_device(24, "trm138", 6, []byte{0x18, 0x04, 0x00, 0x00, 0x00, 0x1E, 0x72, 0x0b}, k2a_3) // панель:1-2N  котёл:2A
+var trm_1 = new_device(8, "trm138", 6, []byte{0x08, 0x04, 0x00, 0x00, 0x00, 0x1e, 0x70, 0x9b}, k1_7) // панель:17-1N  котёл:1
 
-//var trm_2 = new_device(32, "trm138", 6, []byte{0x20, 0x04, 0x00, 0x00, 0x00, 0x1E, 0x76, 0xb3}, k2b_3) // панель:1-3N  котёл:2Б
+var trm_2 = new_device(16, "trm138", 6, []byte{0x10, 0x04, 0x00, 0x00, 0x00, 0x1e, 0x73, 0x43}, k1_8) // панель:2A-1N  котёл:1
 
-//var trm_3 = new_device(40, "trm138", 3, []byte{0x28, 0x04, 0x00, 0x00, 0x00, 0x0f, 0xb7, 0xf7}, k2a_4) // панель:3a-2N  котёл:2A
+var trm_3 = new_device(24, "trm138", 4, []byte{0x18, 0x04, 0x00, 0x00, 0x00, 0x14, 0xf2, 0x0c}, k1_9) // панель:3A-1N  котёл:1
 
-//var trm_4 = new_device(48, "trm138", 2, []byte{0x30, 0x04, 0x00, 0x00, 0x00, 0x0a, 0x74, 0x2c}, k2b_4) // панель:3a-3N  котёл:2Б
+var trm_4 = new_device(8, "trm138", 6, []byte{0x08, 0x04, 0x00, 0x00, 0x00, 0x1e, 0x70, 0x9b}, t1_5) // панель:1-1S  турбина:1
 
-//var trm_5 = new_device(56, "trm138", 8, []byte{0x38, 0x04, 0x00, 0x00, 0x00, 0x28, 0xf5, 0x7d}, t2_8) // панель:2S-2a  турбина:2
+var trm_5 = new_device(16, "trm138", 5, []byte{0x10, 0x04, 0x00, 0x00, 0x00, 0x19, 0x32, 0x81}, t1_6) // панель:1-1S  турбина:1
 
-//var trm_6 = new_device(64, "trm138", 6, []byte{0x40, 0x04, 0x00, 0x00, 0x00, 0x1e, 0x7f, 0x13}, t2_9) // панель:2S-4  турбина:2
+var trm_6 = new_device(24, "trm138", 7, []byte{0x18, 0x04, 0x00, 0x00, 0x00, 0x23, 0xb3, 0xda}, t1_7) // панель:3A-1S  турбина:1
 
 // ========================================================================================================================================================
 
-var Devices = []Device{irt_4}
+var Devices_kettle = []Device{irt_1, irt_2, irt_3, f_1, f_2, tm_1, trm_1, trm_2, trm_3}
+
+var Devices_turbo = []Device{irt_4, irt_5, irt_6, f_3, trm_4, trm_5, trm_6}
--- main.go
+++ main.go
@@ -13,12 +13,19 @@ import (
 
 func main() {
 
-	conn, err := net.Dial("tcp", "192.168.127.254:951")
+	conn_kettle, err := net.Dial("tcp", "192.168.127.254:951")  // порты 951-котёл, 950-турбина 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close()
+	defer conn_kettle.Close()
+
+	conn_turbo, err := net.Dial("tcp", "192.168.127.254:950")  // порты 951-котёл, 950-турбина 
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn_kettle.Close()
 
 	f, err := os.OpenFile("data.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
@@ -33,24 +40,24 @@ func main() {
 
 		t, d := modbus.Today()
 
-		data["T_1time"] = t
+		data["Time"] = t
 		data["Day"] = d
 
-		for _, i := range devices.Devices {
+		for _, i := range devices.Devices_kettle {
 
-			if _, err = conn.Write(i.Request); err != nil {
+			if _, err = conn_kettle.Write(i.Request); err != nil {
 				fmt.Println(err)
 				return
 			}
 
 			buff := make([]byte, 100)
 
-			t := time.NewTimer(1000 * time.Millisecond)
+			t := time.NewTimer(130 * time.Millisecond)
 			<-t.C
 
-			conn.SetReadDeadline(time.Now().Add(time.Millisecond * 1000))
+			conn_kettle.SetReadDeadline(time.Now().Add(time.Millisecond * 130))
 
-			_, err := conn.Read(buff)
+			_, err := conn_kettle.Read(buff)
 			if err != nil {
 				fmt.Println(err)
 				continue
@@ -79,6 +86,60 @@ func main() {
 			}
 
 			fmt.Println(mass_temp) // убрать при окончании настройки
+
+			for j := 0; j < int(i.Qan_chanels); j++ {
+				if mass_temp[j] == float32(math.Inf(1)) {
+					data[i.Chanels[j]] = "Обрыв"
+					continue
+				}
+				data[i.Chanels[j]] = mass_temp[j]
+			}
+
+		}
+
+		for _, i := range devices.Devices_turbo {
+
+			if _, err = conn_turbo.Write(i.Request); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			buff := make([]byte, 100)
+
+			t := time.NewTimer(130 * time.Millisecond)
+			<-t.C
+
+			conn_turbo.SetReadDeadline(time.Now().Add(time.Millisecond * 130))
+
+			_, err := conn_turbo.Read(buff)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			var temp []byte
+			var mass_temp []float32
+
+			switch i.Name {
+			case ("tm5104d"):
+				temp = modbus.Rtu_data_tm5104d(buff, i.Qan_chanels)
+				mass_temp = modbus.Rtu_temp(temp)
+
+			case ("trm138"):
+				temp = modbus.Rtu_data_trm138(buff, i.Qan_chanels)
+				mass_temp = modbus.Rtu_temp(temp)
+
+			case ("irt5920n"):
+				temp = modbus.Ascii_data_itr5920n(buff, i.Id)
+				mass_temp = modbus.Ascii_temp(temp)
+
+			case ("f1772"):
+				temp = modbus.Rtu_data_f1772(buff, i.Qan_chanels)
+				mass_temp = modbus.Rtu_temp(temp)
+
+			}
+
+		//	fmt.Println(mass_temp) // убрать при окончании настройки
 
 			for j := 0; j < int(i.Qan_chanels); j++ {
 				if mass_temp[j] == float32(math.Inf(1)) {
